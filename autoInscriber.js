523f031a4108ce2eb022c6f2c2c1cb6ffb0f9fc4
0xsol1d <93030247+0xsol1d@users.noreply.github.com>
2024-11-01 21:01:15 +0100

    Fixed inscription logic/errors

--- autoInscriber.js
+++ autoInscriber.js
@@ -16,10 +16,10 @@ const end = 8888; // Enter range of files to inscribe to
 let counter = start - 1;
 let txidMatch = "";
 let isPending = false;
+let imgPath = "";
+const commands = [];
 
 function runNodeCommands(start, end, directory, filePrefix, fileExtension) {
-  const commands = [];
-
   for (let i = start; i <= end; i++) {
     const fileNumber = String(i).padStart(5, "0");
     const imagePath = path.join(
@@ -27,39 +27,48 @@ function runNodeCommands(start, end, directory, filePrefix, fileExtension) {
       `${filePrefix}${fileNumber}.${fileExtension}`
     );
 
-    console.log(`Checking file: ${imagePath}`);
-
     if (!fs.existsSync(imagePath)) {
       console.log(`File not found: ${imagePath}`);
       continue;
     }
 
     const baseFileName = path.basename(imagePath, `.${fileExtension}`);
-    console.log(`Base file name: ${baseFileName}`);
 
     commands.push(() => mintFile(baseFileName, imagePath));
   }
-
   executeCommands(commands);
 }
 
 function executeCommands(commands) {
   if (commands.length === 0) return;
 
-  const command = commands[0]; // Peek at the first command, don't shift yet
+  const command = commands[0];
 
   command()
     .then(() => {
       setTimeout(() => {
         commands.shift(); // Only shift if successful
-        executeCommands(commands);
+        syncWallet()
+          .then(() => {
+            executeCommands(commands);
+          })
+          .catch((err) => {
+            console.error("Error syncing wallet:", err);
+            executeCommands(commands);
+          });
       }, 60000); // Wait 60 seconds before executing the next command
     })
     .catch((err) => {
-      console.error("Error executing command:", err);
       // On error, retry the same command after a delay
       setTimeout(() => {
-        executeCommands(commands);
+        syncWallet()
+          .then(() => {
+            executeCommands(commands);
+          })
+          .catch((err) => {
+            console.error("Error syncing wallet:", err);
+            executeCommands(commands);
+          });
       }, 60000); // Wait 60 seconds before retrying the command again
     });
 }
@@ -68,43 +77,55 @@ function mintFile(baseFileName, imagePath) {
   return new Promise((resolve, reject) => {
     const mintCommand = `node bellinals.js mint ${bellsAddress} ${imagePath}`;
     console.log(`Inscribing ${imagePath}`);
+    imgPath = imagePath;
 
     exec(mintCommand, (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
       if (error) {
+        if (
+          stdout.match("Transaction already in block chain") ||
+          stdout.match("bad-txns-inputs-missingorspent") ||
+          stderr.match("Cannot read properties of undefined")
+        ) {
+          fs.unlinkSync("pending-txs.json");
+          console.log("Retrying " + (counter + 1) + " after a few moments...");
+          return reject(error);
+        }
         console.error(`Error in mint command: ${error.message}`);
-        //console.error(`Command: ${mintCommand}`);
-        //console.error(`Stdout: ${stdout}`);
-        //console.error(`Stderr: ${stderr}`);
         if (!isPending) {
-		  console.log(stdout)
           txidMatch = stdout.match(/txid: (\w+)/);
           isPending = true;
         }
+
         return reject(error);
       }
 
-	  if (!isPending) {
-		console.log(stdout)
-		txidMatch = stdout.match(/txid: (\w+)/);
-	  }
+      if (!isPending) {
+        txidMatch = stdout.match(/txid: (\w+)/);
+      }
 
       if (txidMatch) {
         const txid = txidMatch[1];
         console.log(
-          "Successful mint, updating JSON file, continuing in 30 sec...."
+          "Successful mint, updating JSON file, continuing in 60 sec...."
         );
-        isPending = true;
-        updateJsonFile(baseFileName, imagePath, txid)
-          .then(() => {
-            setTimeout(() => {
-              syncWallet().then(resolve).catch(reject);
-            }, 30000); // Wait 30 seconds before syncing the wallet
-          })
-          .catch(reject);
-      } else if (stdout.includes("too long mempool")) {
-        handleMempoolError(stdout).then(resolve).catch(reject);
-      } else if (stdout.includes("18: bad-txns-inputs-spent")) {
-        handleInputsSpentError().then(resolve).catch(reject);
+        isPending = false;
+        if (!fs.existsSync("pending-txs.json")) {
+          updateJsonFile(imgPath, txid).then(resolve).catch(reject);
+        } else {
+          fs.unlinkSync("pending-txs.json");
+          console.log("Error with pending-txs.json");
+          console.log("Retrying " + (counter + 1) + " after a few moments...");
+          return reject(
+            new Error("Error with pending-txs.json, retrying " + (counter + 1))
+          );
+        }
+      } else if (stderr.match("Cannot read properties of undefined")) {
+        fs.unlinkSync("pending-txs.json");
+        console.log("Retrying " + (counter + 1) + " after a few moments...");
+        isPending = false;
+        return reject(error);
       } else {
         console.log("Unknown response, stopping the loop.");
         reject(new Error("Unknown response"));
@@ -113,37 +134,9 @@ function mintFile(baseFileName, imagePath) {
   });
 }
 
-function handleMempoolError(stdout) {
-  return new Promise((resolve, reject) => {
-    const errorType = stdout.includes("64: too-long-mempool-chain")
-      ? "64: too-long-mempool-chain"
-      : "txn-mempool-conflict";
-    console.log(
-      `Detected '${errorType}', running wallet sync and waiting 1 min before retrying...`
-    );
-
-    setTimeout(() => {
-      syncWallet().then(resolve).catch(reject);
-    }, 60000); // Wait 1 minutes before retrying
-  });
-}
-
-function handleInputsSpentError() {
-  return new Promise((resolve, reject) => {
-    console.log(
-      "Detected 'bad-txns-inputs-spent', refreshing UTXOs and waiting 1 min before retrying..."
-    );
-
-    setTimeout(() => {
-      syncWallet().then(resolve).catch(reject);
-    }, 60000); // Wait 1 minutes before retrying
-  });
-}
-
 function syncWallet() {
   return new Promise((resolve, reject) => {
     const syncCommand = `node bellinals wallet sync`;
-    //console.log(`Running command: ${syncCommand}`)
 
     exec(syncCommand, (error, stdout, stderr) => {
       if (error) {
@@ -153,16 +146,33 @@ function syncWallet() {
         console.error(`Stderr: ${stderr}`);
         return reject(error);
       }
-
-      //console.log('Output from wallet sync command:')
-      //console.log(stdout);
-      console.log("\nwaiting a few moments till next inscription command");
-      resolve();
+      console.log(`${stdout}`);
+      if (stdout.match("inscription complete continue to next file.")) {
+        const txid = txidMatch[1];
+        isPending = false;
+        if (!fs.existsSync("pending-txs.json")) {
+          updateJsonFile(imgPath, txid)
+            .then(() => {
+              commands.shift();
+              resolve();
+            })
+            .catch((err) => {
+              console.error(`Error updating JSON file: ${err.message}`);
+              return reject(err); 
+            });
+        } else {
+          return reject(
+            new Error("Error with pending-txs.json, retrying " + (counter + 1))
+          );
+        }
+      } else {
+        resolve();
+      }
     });
   });
 }
 
-function updateJsonFile(baseFileName, imagePath, txid) {
+function updateJsonFile(imagePath, txid) {
   return new Promise((resolve, reject) => {
     const jsonDir = "build";
     if (!fs.existsSync(jsonDir)) {
@@ -185,24 +195,7 @@ function updateJsonFile(baseFileName, imagePath, txid) {
       "with id",
       txid + "i0"
     );
-    console.log("\nwaiting for wallet sync");
     fs.writeFileSync(jsonFileName, JSON.stringify(data, null, 4));
-
-    const newJsonFileName = path.join(jsonDir, "updated_inscriptions.json");
-    let updatedData = [];
-    if (fs.existsSync(newJsonFileName)) {
-      updatedData = JSON.parse(fs.readFileSync(newJsonFileName, "utf-8"));
-    }
-    fs.writeFileSync(newJsonFileName, JSON.stringify(data, null, 4));
-
-    const newJsonFileName2 = path.join(jsonDir, "updated_inscriptions2.json");
-    let updatedData2 = [];
-    if (fs.existsSync(newJsonFileName2)) {
-      updatedData2 = JSON.parse(fs.readFileSync(newJsonFileName2, "utf-8"));
-    }
-    updatedData2.push({ id: txid + "i0", imagePath: imagePath });
-    fs.writeFileSync(newJsonFileName2, JSON.stringify(updatedData2, null, 4));
-
     resolve();
   });
 }
